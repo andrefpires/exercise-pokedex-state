aa0c7bb0e928d85c41c8521169ae36d85e5a8402
André Ferreira <andrefpires1@gmail.com>
2020-11-29 03:26:15 -0300

    Exercício 1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <h1>My pokedex</h1>
       </header>
         <Pokedex />
-    </div>
+      </div>
   );
 }
 
--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -3,11 +3,35 @@ import Pokemon from './Pokemon';
 import pokemons from '../data';
 
 export default class Pokedex extends Component {
+  constructor() {
+    super();
+    this.state = {
+      countButton: 0,
+    }
+
+    this.passPokemon = this.passPokemon.bind(this);
+  }
+
+  passPokemon() {
+    if(this.state.countButton < 8) {
+      this.setState((oldState, _props) => ({
+        countButton: oldState.countButton + 1,
+      }));
+    } else {
+      this.setState({
+        countButton: 0,
+      })
+    }
+  }
+
   render() {
-    const allPokemonsInformations = pokemons.map((element) => <Pokemon key={element.name} pokemon={element} />)
+    let count = this.state.countButton;
     return (
-      <div className="pokedex">
-        { allPokemonsInformations }
+      <div>
+        <div className="pokedex">
+          <Pokemon key={pokemons[count].name} pokemon={pokemons[count]} />
+        </div>
+        <button onClick={this.passPokemon}>Pass Pokemon</button>
       </div>
     )
   }
