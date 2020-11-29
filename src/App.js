750e93e5169cf8104470c65dfd8b855cd2341ee9
André Ferreira <andrefpires1@gmail.com>
2020-11-29 01:59:11 -0300

    Copiando diretório src do bloco anterior e mudanças no package.json

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import Pokedex from './components/Pokedex';
 import './App.css';
-import pokemons from './data';
-import Pokedex from './Pokedex';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
+    <div>
+      <header>
+        <h1>My pokedex</h1>
+      </header>
+        <Pokedex />
     </div>
   );
 }
